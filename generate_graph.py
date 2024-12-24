1074b5b704197005b8cd98cd60d032a70bbbec6e
vishal-s-patil <vishalspolicepatil55@gmail.com>
2024-12-24 10:37:05 +0530

    updated

--- generate_graph.py
+++ generate_graph.py
@@ -24,11 +24,11 @@ def generate_image(x, y):
     return image_binary
 
 def create_combined_graph(x, y, user_count_map, title, x_axis, y_axis, file_name="combined_graph.png"):
-    plt.bar(x, y, label="Bar Graph (Y)", color='skyblue', alpha=0.7)
+    plt.bar(x, y, label="y_axis", color='skyblue', alpha=0.7)
     
     colors = ['red', 'blue', 'green', 'orange', 'purple', 'brown', 'pink', 'gray', 'black', 'yellow', 'cyan', 'magenta', 'r', 'g', 'b', 'c', 'm', 'y', 'k', 'w', '#FF5733', '#33FF57', '#5733FF', 'aqua', 'azure', 'beige', 'chocolate', 'coral', 'gold', 'indigo', 'khaki', 'lavender', 'navy', 'olive', 'teal']
     idx = 0
-    for user, lst in user_count_map:
+    for user, lst in user_count_map.items():
         plt.plot(x, lst, label=user, color=colors[idx], marker='o')
         idx+=1
     
@@ -46,13 +46,13 @@ def create_combined_graph(x, y, user_count_map, title, x_axis, y_axis, file_name
     
     return img_buffer.getvalue()
 
-if __name__ == '__main__':
-    title = "All selects day wise trend for 4 weeks"
-    x_axis = "day"
-    y_axis = "total_selects"
+# if __name__ == '__main__':
+#     title = "All selects day wise trend for 4 weeks"
+#     x_axis = "day"
+#     y_axis = "total_selects"
 
-    x = [1, 2, 3, 4, 5]
-    y = [10, 20, 30, 20, 5]
-    z = [3, 7, 4, 8, 3]
+#     x = [1, 2, 3, 4, 5]
+#     y = [10, 20, 30, 20, 5]
+#     z = [3, 7, 4, 8, 3]
 
-    create_combined_graph(x, y, z, title, x_axis, y_axis)
+#     create_combined_graph(x, y, z, title, x_axis, y_axis)
