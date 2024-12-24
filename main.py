8e939050f5da9e698158e23adc71d1da9dfcf358
vishal-s-patil <vishalspolicepatil55@gmail.com>
2024-12-24 10:41:16 +0530

    updated

--- main.py
+++ main.py
@@ -27,13 +27,12 @@ vertica_config = {
 
 
 if __name__ == "__main__":
-    print("Generation graphs")
-
     vertica_connection = create_connection(vertica_config["host"], vertica_config["user"], vertica_config["password"], vertica_config["database"], vertica_config["port"], vertica_config["autoCommit"])
 
     opperations = ['SELECT', 'COPY', 'INSERT', 'UPDATE', 'DELETE', 'MERGE']
     users = ['behaviour', 'campaign_listing'] # 'contact_summary', 'raman',
     title_image_pairs = []
+    user_count_map = {}
     for opperation in opperations:
         query = f"""select
             date_trunc('day', date_trunc_time::timestamp) as date_trunc_day,
@@ -43,7 +42,6 @@ if __name__ == "__main__":
             group by date_trunc_day 
             order by date_trunc_day;"""
         
-        user_count_map = {}
         for user in users:
             query_with_user = f"""select
             date_trunc('day', date_trunc_time::timestamp) as date_trunc_day,
@@ -54,12 +52,10 @@ if __name__ == "__main__":
             order by date_trunc_day;"""
             result = read(vertica_connection, query_with_user, ["date", "count"])
             user_count_map[user] = result['count'].to_list()
-            print(user, len(user_count_map[user]))
         
         columns = ["date", "count"]
 
         df = read(vertica_connection, query, columns)
-        # print(df)
 
         title = f"All {opperation} day wise trend for 4 weeks"
         x_axis = "day"
@@ -67,8 +63,5 @@ if __name__ == "__main__":
 
         img = create_combined_graph(df["date"].to_list(), df["count"].to_list(), user_count_map, title, x_axis, y_axis)
         title_image_pairs.append((title, img))
+    
     send_email_with_titles_and_images(title_image_pairs, mail_config)
-
-'''
-
-'''
