86340725af685cac898d428d273cd434c10b200a
vishal-s-patil <vishalspolicepatil55@gmail.com>
2024-12-24 16:37:54 +0530

    updated

--- main.py
+++ main.py
@@ -101,8 +101,6 @@ def plot_exec_time_graph(vertica_connection, opperations, users):
         x = list(map(lambda ts: ts.day, df['date'].to_list()))
         x = list(map(lambda day: str(day), x))
         if opperation == 'SELECT':
-            print(user_count_map)
-            exit()
             img = create_combined_graph(x, df["count"].to_list(), user_count_map, title, x_axis, y_axis)
         else:
             user_count_map = {}
