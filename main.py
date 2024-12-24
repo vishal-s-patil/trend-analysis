d95ca195f2a3105ec9addd8b96c3d83a4654db78
vishal-s-patil <vishalspolicepatil55@gmail.com>
2024-12-24 10:23:39 +0530

    updated

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
             user_count_map[user] = result
         
         print(user_count_map)
-        return
+        exit()
 
         columns = ["date", "count"]
 
