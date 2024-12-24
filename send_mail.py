b3d238984135e1c008335c57f179f5c1297e381b
vishal-s-patil <vishalspolicepatil55@gmail.com>
2024-12-24 16:57:01 +0530

    updated

--- send_mail.py
+++ send_mail.py
@@ -11,23 +11,23 @@ def send_email_with_titles_and_images(lst_title_image_pairs, mail_config, items_
     msg['Subject'] = subject
 
     body = "<html><body>"
-    body += "<table style='width:100%;'>"
-    
-    for (heading, title_image_pairs) in lst_title_image_pairs:
-        print("heading", heading)
-        body += f"<h2>{heading}</h2>"
+
+    for heading, title_image_pairs in lst_title_image_pairs:
+        body += f"<h2 style='text-align:center;'>{heading}</h2>"
+        body += "<table style='width:100%; border-collapse:collapse;'>"
+        
         for idx, (title, img) in enumerate(title_image_pairs, start=1):
             if (idx - 1) % items_per_row == 0:
                 body += "<tr>"
             
             body += f"<td style='width:{100 // items_per_row}%; padding:10px; text-align:center;'>"
-            body += f"<h2>{title}</h2>"
+            body += f"<h4>{title}</h4>"
             
             img = MIMEImage(img, _subtype='png')
-            img.add_header('Content-ID', f'<image{idx}>')
+            img.add_header('Content-ID', f'<image{heading}_{idx}>')
             msg.attach(img)
 
-            body += f'<img src="cid:image{idx}" alt="Image {idx}" style="width:300px;height:auto;">'
+            body += f'<img src="cid:image{heading}_{idx}" alt="Image {idx}" style="width:300px;height:auto;">'
             body += "</td>"
 
             if idx % items_per_row == 0:
@@ -36,10 +36,11 @@ def send_email_with_titles_and_images(lst_title_image_pairs, mail_config, items_
         if len(title_image_pairs) % items_per_row != 0:
             body += "</tr>"
         
-        body += "</table>"
-        body += "</body></html>"
+        body += "</table><br>"
 
-        msg.attach(MIMEText(body, 'html'))
+    body += "</body></html>"
+
+    msg.attach(MIMEText(body, 'html'))
 
     try:
         with smtplib.SMTP(mail_config["smtp_server"], mail_config["smtp_port"]) as server:
@@ -49,6 +50,7 @@ def send_email_with_titles_and_images(lst_title_image_pairs, mail_config, items_
             print("Email sent successfully!")
     except Exception as e:
         print(f"Failed to send email: {e}")
+
 
 if __name__ == "__main__":
     pass
