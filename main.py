2c2923083a14d618f430c0df5c392d1a1321d3e9
vishal-s-patil <vishalspolicepatil55@gmail.com>
2024-12-24 10:23:24 +0530

    updated

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ if __name__ == "__main__":
     vertica_connection = create_connection(vertica_config["host"], vertica_config["user"], vertica_config["password"], vertica_config["database"], vertica_config["port"], vertica_config["autoCommit"])
 
     opperations = ['SELECT', 'COPY', 'INSERT', 'UPDATE', 'DELETE', 'MERGE']
+    users = ['behaviour', 'campaign_listing'] # 'contact_summary', 'raman',
     title_image_pairs = []
     for opperation in opperations:
         query = f"""select
@@ -41,6 +42,21 @@ if __name__ == "__main__":
             where date_trunc_day >= '2024-11-30' and operation = '{opperation[0]}'
             group by date_trunc_day 
             order by date_trunc_day;"""
+        
+        user_count_map = {}
+        for user in users:
+            query_with_user = f"""select
+            date_trunc('day', date_trunc_time::timestamp) as date_trunc_day,
+            count(1)
+            from netstats.trend_analysis 
+            where date_trunc_day >= '2024-11-30' and operation = '{opperation[0]}' and user_name = {user}
+            group by date_trunc_day 
+            order by date_trunc_day;"""
+            result = read(vertica_connection, query_with_user, ["date", "count"])
+            user_count_map[user] = result
+        
+        print(user_count_map)
+        return
 
         columns = ["date", "count"]
 
@@ -54,3 +70,7 @@ if __name__ == "__main__":
         img = create_combined_graph(df["date"].to_list(), df["count"].to_list(), df["count"].to_list(), title, x_axis, y_axis)
         title_image_pairs.append((title, img))
     send_email_with_titles_and_images(title_image_pairs, mail_config)
+
+'''
+
+'''
