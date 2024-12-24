af2f13ae4673628abab13c304b2a89420b528701
vishal-s-patil <vishalspolicepatil55@gmail.com>
2024-12-24 17:13:23 +0530

    updated

--- send_mail.py
+++ send_mail.py
@@ -11,34 +11,29 @@ def send_email_with_titles_and_images(lst_title_image_pairs, mail_config, items_
     msg['Subject'] = subject
 
     body = "<html><body>"
-    
-    for (heading, title_image_pairs) in lst_title_image_pairs:
-        body += f"<h2>{heading}</h2>"
-        body += "<table style='width:100%;'>"
-        print(len(title_image_pairs))
+
+    for heading, title_image_pairs in lst_title_image_pairs:
+        body += f"<h2 style='text-align:center;'>{heading}</h2>"
+        body += "<table style='width:100%; border-collapse:collapse;'>"
+        
         for idx, (title, img) in enumerate(title_image_pairs, start=1):
             if (idx - 1) % items_per_row == 0:
                 body += "<tr>"
             
-            body += f"<td style='width:{100 // items_per_row}%; padding:10px; text-align:center;'>"
-            body += f"<h2>{title}</h2>"
-            
+            body += f"<td style='width:{100 // items_per_row}%; text-align:center; padding:10px;'>"
+            body += f"<h4>{title}</h4>"
             img = MIMEImage(img, _subtype='png')
-            img.add_header('Content-ID', f'<image{idx}>')
-            # msg.attach(img)
-
-            body += f'<img src="cid:image{idx}" alt="Image {idx}" style="width:300px;height:auto;">'
+            img.add_header('Content-ID', f'<image{heading}_{idx}>')
+            msg.attach(img)
+            body += f'<img src="cid:image{heading}_{idx}" alt="Image {idx}" style="width:300px;height:auto;">'
             body += "</td>"
 
             if idx % items_per_row == 0:
-                body += "</tr>"
-        body += "</table>"
-        
-        if len(title_image_pairs) % items_per_row != 0:
-            body += "</tr>"
-        
-    body += "</body></html>"
+                body += "</tr><tr>"  # Close the current row and prepare for the next set of titles/images
+            
+        body += "</tr></table><br>"
 
+    body += "</body></html>"
     msg.attach(MIMEText(body, 'html'))
 
     try:
