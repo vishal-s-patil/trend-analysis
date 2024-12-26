9d3388f5deecac90012c700e4f1f14f145ba35b1
vishal-s-patil <vishalspolicepatil55@gmail.com>
2024-12-26 15:54:08 +0530

    updated

--- main.py
+++ main.py
@@ -114,13 +114,11 @@ def plot_exec_time_graph_day(vertica_connection, opperations, users):
         x = list(map(lambda day: str(day), x))
         if opperation == 'SELECT':
             for user, user_list in user_count_map.items():
-                print('user', len(x), len(user_list))
                 if len(user_list) > len(x):
                     diff = len(user_list) - len(x)
                     while diff > 0:
                         user_list.pop(-1)
                         diff -= diff
-                    print(len(x), len(user_list))
             user_count_map = {}
             img = create_combined_graph(x, df["count"].to_list(), user_count_map, title, x_axis, y_axis)
         else:
