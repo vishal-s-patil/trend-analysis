ec30adeb79f82610a5d9eef15ff88a6130c73107
vishal-s-patil <vishalspolicepatil55@gmail.com>
2024-12-27 12:30:43 +0530

    updated

--- main.py
+++ main.py
@@ -84,55 +84,55 @@ def get_day_wise_dimensions_performance(opperation, args):
     for user in args['users']:
         user_count_map[user] = [0] * 100
 
-        if args['days'] == 0:
-            query = f"""select
+    if args['days'] == 0:
+        query = f"""select
+            date_trunc('day', date_trunc_time::timestamp) as date_trunc_day,
+            avg(avg_duration_ms) as avg_duration_ms
+            from netstats.trend_analysis 
+            where date_trunc_day > '{args['from_datetime']}' and date_trunc_day <= '{args['to_datetime']}' and operation = '{opperation[0]}'
+            group by date_trunc_day 
+            order by date_trunc_day;"""
+    else:
+        query = f"""select
+            date_trunc('day', date_trunc_time::timestamp) as date_trunc_day,
+            avg(avg_duration_ms) as avg_duration_ms
+            from netstats.trend_analysis 
+            where date_trunc_day > '{get_past_date(args['days'], args['to_datetime'])}' and date_trunc_day <= '{args['to_datetime']}' and operation = '{opperation[0]}'
+            group by date_trunc_day 
+            order by date_trunc_day;"""
+    if opperation == 'SELECT':
+        for user in args['users']:
+            if args['days'] == 0:
+                query_with_user = f"""select
                 date_trunc('day', date_trunc_time::timestamp) as date_trunc_day,
                 avg(avg_duration_ms) as avg_duration_ms
                 from netstats.trend_analysis 
-                where date_trunc_day > '{args['from_datetime']}' and date_trunc_day <= '{args['to_datetime']}' and operation = '{opperation[0]}'
+                where date_trunc_day > '{args['from_datetime']}' and date_trunc_day <= '{args['from_datetime']}' and operation = '{opperation[0]}' and user_name = '{user}'
                 group by date_trunc_day 
                 order by date_trunc_day;"""
-        else:
-            query = f"""select
+            else:
+                query_with_user = f"""select
                 date_trunc('day', date_trunc_time::timestamp) as date_trunc_day,
                 avg(avg_duration_ms) as avg_duration_ms
                 from netstats.trend_analysis 
-                where date_trunc_day > '{get_past_date(args['days'], args['to_datetime'])}' and date_trunc_day <= '{args['to_datetime']}' and operation = '{opperation[0]}'
+                where date_trunc_day > '{get_past_date(args['days'], args['to_datetime'])}' and date_trunc_day <= '{args['to_datetime']}' and operation = '{opperation[0]}' and user_name = '{user}'
                 group by date_trunc_day 
                 order by date_trunc_day;"""
-        if opperation == 'SELECT':
-            for user in args['users']:
-                if args['days'] == 0:
-                    query_with_user = f"""select
-                    date_trunc('day', date_trunc_time::timestamp) as date_trunc_day,
-                    avg(avg_duration_ms) as avg_duration_ms
-                    from netstats.trend_analysis 
-                    where date_trunc_day > '{args['from_datetime']}' and date_trunc_day <= '{args['from_datetime']}' and operation = '{opperation[0]}' and user_name = '{user}'
-                    group by date_trunc_day 
-                    order by date_trunc_day;"""
-                else:
-                    query_with_user = f"""select
-                    date_trunc('day', date_trunc_time::timestamp) as date_trunc_day,
-                    avg(avg_duration_ms) as avg_duration_ms
-                    from netstats.trend_analysis 
-                    where date_trunc_day > '{get_past_date(args['days'], args['to_datetime'])}' and date_trunc_day <= '{args['to_datetime']}' and operation = '{opperation[0]}' and user_name = '{user}'
-                    group by date_trunc_day 
-                    order by date_trunc_day;"""
-                
-                result = read(args['vertica_connection'], query_with_user, ["date", "count"])
-                for i, cnt in enumerate(result['count'].to_list()):
-                    user_count_map[user][i] = cnt
+            
+            result = read(args['vertica_connection'], query_with_user, ["date", "count"])
+            for i, cnt in enumerate(result['count'].to_list()):
+                user_count_map[user][i] = cnt
 
-        columns = ["date", "count"]
+    columns = ["date", "count"]
 
-        df = read(vertica_connection, query, columns)
+    df = read(vertica_connection, query, columns)
 
-        x = list(map(lambda ts: ts.day, df['date'].to_list()))
-        x = list(map(lambda day: str(day), x))
+    x = list(map(lambda ts: ts.day, df['date'].to_list()))
+    x = list(map(lambda day: str(day), x))
 
-        y = df["count"].to_list()
+    y = df["count"].to_list()
 
-        return {'x':x, 'y':y, 'user_count_map':user_count_map}
+    return {'x':x, 'y':y, 'user_count_map':user_count_map}
 
 
 def plot_exec_time_graph_day(args):
