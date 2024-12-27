f11131152f4ff967ca657ecdd2c471402264d5ed
vishal-s-patil <vishalspolicepatil55@gmail.com>
2024-12-27 11:25:24 +0530

    updated

--- main.py
+++ main.py
@@ -31,11 +31,11 @@ def get_past_date(days_ago, start_date):
     return past_date.strftime('%Y-%m-%d')
 
 
-def plot_count_graph_day(agrs):
+def plot_count_graph_day(args):
     title_image_pairs = []
     user_count_map = {}
 
-    for opperation in agrs['opperations']:
+    for opperation in args['opperations']:
         if args['days'] == 0:
             query = f"""select
                 date_trunc('day', date_trunc_time::timestamp) as date_trunc_day,
@@ -56,7 +56,7 @@ def plot_count_graph_day(agrs):
         
         columns = ["date", "count"]
 
-        df = read(agrs['vertica_connection'], query, columns)
+        df = read(args['vertica_connection'], query, columns)
 
         title = f"{opperation}"
         x_axis = "day"
@@ -70,20 +70,20 @@ def plot_count_graph_day(agrs):
     
     return title_image_pairs
 
-def plot_exec_time_graph_day(agrs):
+def plot_exec_time_graph_day(args):
     user_count_map = {}
     title_image_pairs = []
 
-    for user in agrs['users']:
+    for user in args['users']:
         user_count_map[user] = [0] * 100
 
-    for opperation in agrs['opperations']:
+    for opperation in args['opperations']:
         if args['days'] == 0:
             query = f"""select
                 date_trunc('day', date_trunc_time::timestamp) as date_trunc_day,
                 avg(avg_duration_ms) as avg_duration_ms
                 from netstats.trend_analysis 
-                where date_trunc_day > '{agrs['from_datetime']}' and date_trunc_day <= '{agrs['to_datetime']}' and operation = '{opperation[0]}'
+                where date_trunc_day > '{args['from_datetime']}' and date_trunc_day <= '{args['to_datetime']}' and operation = '{opperation[0]}'
                 group by date_trunc_day 
                 order by date_trunc_day;"""
         else:
@@ -91,17 +91,17 @@ def plot_exec_time_graph_day(agrs):
                 date_trunc('day', date_trunc_time::timestamp) as date_trunc_day,
                 avg(avg_duration_ms) as avg_duration_ms
                 from netstats.trend_analysis 
-                where date_trunc_day > '{get_past_date(args['days'], args['to_datetime'])}' and date_trunc_day <= '{agrs['to_datetime']}' and operation = '{opperation[0]}'
+                where date_trunc_day > '{get_past_date(args['days'], args['to_datetime'])}' and date_trunc_day <= '{args['to_datetime']}' and operation = '{opperation[0]}'
                 group by date_trunc_day 
                 order by date_trunc_day;"""
         if opperation == 'SELECT':
-            for user in agrs['users']:
+            for user in args['users']:
                 if args['days'] == 0:
                     query_with_user = f"""select
                     date_trunc('day', date_trunc_time::timestamp) as date_trunc_day,
                     avg(avg_duration_ms) as avg_duration_ms
                     from netstats.trend_analysis 
-                    where date_trunc_day > '{agrs['from_datetime']}' and date_trunc_day <= '{agrs['from_datetime']}' and operation = '{opperation[0]}' and user_name = '{user}'
+                    where date_trunc_day > '{args['from_datetime']}' and date_trunc_day <= '{args['from_datetime']}' and operation = '{opperation[0]}' and user_name = '{user}'
                     group by date_trunc_day 
                     order by date_trunc_day;"""
                 else:
@@ -109,7 +109,7 @@ def plot_exec_time_graph_day(agrs):
                     date_trunc('day', date_trunc_time::timestamp) as date_trunc_day,
                     avg(avg_duration_ms) as avg_duration_ms
                     from netstats.trend_analysis 
-                    where date_trunc_day > '{get_past_date(args['days'], args['to_datetime'])}' and date_trunc_day <= '{agrs['to_datetime']}' and operation = '{opperation[0]}' and user_name = '{user}'
+                    where date_trunc_day > '{get_past_date(args['days'], args['to_datetime'])}' and date_trunc_day <= '{args['to_datetime']}' and operation = '{opperation[0]}' and user_name = '{user}'
                     group by date_trunc_day 
                     order by date_trunc_day;"""
                 
