f0112bd9371f831efc8acecb3b528f1870d53f16
vishal-s-patil <vishalspolicepatil55@gmail.com>
2024-12-28 17:18:33 +0530

    updated

--- modules/plot_count_graphs.py
+++ modules/plot_count_graphs.py
@@ -5,12 +5,12 @@ from .helpers import get_past_date
 def plot_count_graph_day(args):
     title_image_pairs = []
 
-    for opperation in args['opperations']:
-        title = f"{opperation}"
+    for operation in args['operations']:
+        title = f"{operation}"
         x_axis = "day"
         y_axis = f"count"
 
-        dimensions_count = get_day_wise_dimensions_count(opperation, args)
+        dimensions_count = get_day_wise_dimensions_count(operation, args)
         
         img = create_combined_graph(dimensions_count['x'], dimensions_count['y'], dimensions_count['user_count_map'], title, x_axis, y_axis)
         title_image_pairs.append((title, img))
@@ -18,7 +18,7 @@ def plot_count_graph_day(args):
     return title_image_pairs
 
 
-def get_day_wise_dimensions_count(opperation, args):
+def get_day_wise_dimensions_count(operation, args):
     user_count_map = {}
 
     if args['days'] == 0:
@@ -26,7 +26,7 @@ def get_day_wise_dimensions_count(opperation, args):
             date_trunc('day', date_trunc_time::timestamp) as date_trunc_day,
             count(1)
             from netstats.trend_analysis 
-            where date_trunc_day > '{args['from_datetime']}' and date_trunc_day <= '{args['to_datetime']}' and operation = '{opperation[0]}'
+            where date_trunc_day > '{args['from_datetime']}' and date_trunc_day <= '{args['to_datetime']}' and operation = '{operation[0]}'
             group by date_trunc_day 
             order by date_trunc_day;"""
     else:
@@ -34,7 +34,7 @@ def get_day_wise_dimensions_count(opperation, args):
             date_trunc('day', date_trunc_time::timestamp) as date_trunc_day,
             count(1)
             from netstats.trend_analysis 
-            where date_trunc_day > '{get_past_date(args['days'], args['to_datetime'])}' and date_trunc_day <= '{args['to_datetime']}' and operation = '{opperation[0]}'
+            where date_trunc_day > '{get_past_date(args['days'], args['to_datetime'])}' and date_trunc_day <= '{args['to_datetime']}' and operation = '{operation[0]}'
             group by date_trunc_day 
             order by date_trunc_day;"""
     
