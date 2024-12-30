b8f6365459470ec477aa9e94bf70cf131461bb22
vishal-s-patil <vishalspolicepatil55@gmail.com>
2024-12-30 15:00:30 +0530

    updated

--- modules/send_mail.py
+++ modules/send_mail.py
@@ -23,7 +23,7 @@ def send_email_with_titles_and_images(lst_title_image_pairs, mail_config, items_
             body += f"<td style='width:{100 // items_per_row}%; text-align:center; padding:10px;'>"
             body += f"<h4>{title}</h4>"
             img = MIMEImage(img, _subtype='png')
-            content_id = f'image{heading.replace(' ', '_')}_{idx}'
+            content_id = f'image{heading.replace(" ", "_")}_{idx}'
             img.add_header('Content-ID', content_id)  # f'<image{heading.split(" ")[1]}_{idx}>'
             msg.attach(img)
             body += f'<img src="cid:{content_id}" alt="Image {idx}" style="width:300px;height:auto;">'
