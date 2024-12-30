e3ab9a5be499da336377f95662730028be33e7fe
vishal-s-patil <vishalspolicepatil55@gmail.com>
2024-12-30 12:52:42 +0530

    updated

--- modules/generate_graph.py
+++ modules/generate_graph.py
@@ -34,8 +34,8 @@ def create_combined_graph(x, y, user_count_map, title, x_axis, y_axis, file_name
             plt.plot(x, lst, label=user, color=colors[idx], marker='o')
             idx += 1
 
-    step = max(len(x) // 5, 1)
-    plt.xticks(ticks=range(0, len(x), step), labels=[x[i] for i in range(0, len(x), step)])
+    # step = max(len(x) // 5, 1)
+    # plt.xticks(ticks=range(0, len(x), step), labels=[x[i] for i in range(0, len(x), step)])
     
     # plt.title(title)
     plt.xlabel(x_axis)
