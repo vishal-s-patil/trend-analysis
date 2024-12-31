ef36cd6fbb96de4caeb09c96578cb788b2f95e0d
vishal-s-patil <vishalspolicepatil55@gmail.com>
2024-12-31 14:53:22 +0530

    updated

--- main.py
+++ main.py
@@ -197,13 +197,13 @@ def send_month_wise_graphs(vertica_connection):
 
 
 def send_day_wise_graphs(vertica_connection):
-    to_datetime = '2024-11-26 00:00'
+    to_datetime = '2024-11-27 00:00'
     args = {
         'operations': ['SELECT', 'COPY', 'INSERT', 'UPDATE', 'DELETE', 'MERGE'],
         'users': ['contact_summary', 'sas', 'campaign_listing', 'campaign_report'],
         'vertica_connection': vertica_connection,
         # 'from_datetime': '2024-11-21',
-        'to_datetime': '2024-11-26',
+        'to_datetime': '2024-11-27',
         'days': 5,
     }
 
