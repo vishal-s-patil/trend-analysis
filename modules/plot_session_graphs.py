b48519743a4e76061d42ac25dd2768d5ea6dc46b
vishal-s-patil <vishalspolicepatil55@gmail.com>
2024-12-31 14:02:49 +0530

    updated

--- modules/plot_session_graphs.py
+++ modules/plot_session_graphs.py
@@ -10,7 +10,7 @@ def get_hour_wise_dimensions_session(args):
         query = f"""
                 select date_trunc('min', snapshot_time::timestamp) as min_date_trunc, count(1)
                 from netstats.sessions_full
-                where snapshot_time >= '{from_time}'
+                where snapshot_time >= '{from_time}' and snapshot_time <= {args['to_datetime']}
                 group by min_date_trunc
                 order by min_date_trunc;
                 """
